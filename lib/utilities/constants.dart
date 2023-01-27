5002df667087e97986c444640b54067ba5fc9b12
Joni Tiainen <jonitia91@gmail.com>
2023-01-27 20:55:45 +0200

    Update constants.dart

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 // Colors
+// rgb(29,56,58)
+// rgb(50,168,180)
+// rgb(76,39,89)
+// rgb(50,32,70)
+// rgb(27,195,184)
 const kBackgroundColor = Color.fromRGBO(16, 10, 30, 1);
 const kBackgroundColorLight = Color.fromARGB(255, 30, 19, 56);
 const kMotivationCardColor = Color.fromRGBO(21, 28, 39, 1);
